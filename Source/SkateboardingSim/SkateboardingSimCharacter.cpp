0e535af6162ed504bafa4c457cfc9d65e3be46cb
Juan Sebastian Cadavid Carcamo <juansecadavid@gmail.com>
2024-07-07 18:05:07 -0500

    Last Minute Bug Fixes
    Some details were fixed:
    Inputs fix to slow down correctly
    Music Added

--- Source/SkateboardingSim/SkateboardingSimCharacter.cpp
+++ Source/SkateboardingSim/SkateboardingSimCharacter.cpp
@@ -1,5 +1,3 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
 #include "SkateboardingSimCharacter.h"
 #include "Engine/LocalPlayer.h"
 #include "Camera/CameraComponent.h"
@@ -18,257 +16,208 @@ DEFINE_LOG_CATEGORY(LogTemplateCharacter);
 //////////////////////////////////////////////////////////////////////////
 // ASkateboardingSimCharacter
 
+
+// Initializes the character with default settings for movement and camera components.
 ASkateboardingSimCharacter::ASkateboardingSimCharacter()
 {
-	//CustomRootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("CustomRootComponent"));
-	//RootComponent = CustomRootComponent;
+    GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
+        
+    bUseControllerRotationPitch = false;
+    bUseControllerRotationYaw = false;
+    bUseControllerRotationRoll = false;
 
-	// Mover el CapsuleComponent para que ya no sea el componente raíz
-	//GetCapsuleComponent()->SetupAttachment(CustomRootComponent);
-	// Set size for collision capsule
-	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
-		
-	// Don't rotate when the controller rotates. Let that just affect the camera.
-	bUseControllerRotationPitch = false;
-	bUseControllerRotationYaw = false;
-	bUseControllerRotationRoll = false;
+    GetCharacterMovement()->bOrientRotationToMovement = true;
+    GetCharacterMovement()->RotationRate = FRotator(0.0f, 500.0f, 0.0f);
 
-	// Configure character movement
-	GetCharacterMovement()->bOrientRotationToMovement = true; // Character moves in the direction of input...	
-	GetCharacterMovement()->RotationRate = FRotator(0.0f, 500.0f, 0.0f); // ...at this rotation rate
+    GetCharacterMovement()->JumpZVelocity = 700.f;
+    GetCharacterMovement()->AirControl = 0.35f;
+    GetCharacterMovement()->MaxWalkSpeed = 500.f;
+    GetCharacterMovement()->MinAnalogWalkSpeed = 20.f;
+    GetCharacterMovement()->BrakingDecelerationWalking = 2000.f;
+    GetCharacterMovement()->BrakingDecelerationFalling = 1500.0f;
 
-	// Note: For faster iteration times these variables, and many more, can be tweaked in the Character Blueprint
-	// instead of recompiling to adjust them
-	GetCharacterMovement()->JumpZVelocity = 700.f;
-	GetCharacterMovement()->AirControl = 0.35f;
-	GetCharacterMovement()->MaxWalkSpeed = 500.f;
-	GetCharacterMovement()->MinAnalogWalkSpeed = 20.f;
-	GetCharacterMovement()->BrakingDecelerationWalking = 2000.f;
-	GetCharacterMovement()->BrakingDecelerationFalling = 1500.0f;
+    CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+    CameraBoom->SetupAttachment(RootComponent);
+    CameraBoom->TargetArmLength = 400.0f;
+    CameraBoom->bUsePawnControlRotation = true;
 
-	// Create a camera boom (pulls in towards the player if there is a collision)
-	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
-	CameraBoom->SetupAttachment(RootComponent);
-	CameraBoom->TargetArmLength = 400.0f; // The camera follows at this distance behind the character	
-	CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
-
-	// Create a follow camera
-	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
-	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
-	FollowCamera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
-
-	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
-	// are set in the derived blueprint asset named ThirdPersonCharacter (to avoid direct content references in C++)
+    FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+    FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+    FollowCamera->bUsePawnControlRotation = false;
 }
 
+// Called when the game starts or when spawned. Initializes the SkateBoard component and sets up the input mapping context.
 void ASkateboardingSimCharacter::BeginPlay()
 {
-	// Call the base class  
-	Super::BeginPlay();
+    Super::BeginPlay();
 
-	SkateBoard = Cast<UStaticMeshComponent>(GetDefaultSubobjectByName(TEXT("SkateStaticMesh")));
-	//Add Input Mapping Context
-	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
-	{
-		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
-		{
-			Subsystem->AddMappingContext(DefaultMappingContext, 0);
-		}
-	}
-	
-	// Obtener la inclinación del plano
-	FVector FloorNormal = GetCharacterMovement()->CurrentFloor.HitResult.ImpactNormal;
-	float FloorAngle = FMath::RadiansToDegrees(FMath::Acos(FloorNormal.Z));
-
-	// Aplicar fuerza adicional si el personaje está en una pendiente
-	if (FloorAngle > 0)
-	{
-		FVector SlideDirection = FVector(FloorNormal.X, FloorNormal.Y, 0.0f).GetSafeNormal();
-		FVector Force = SlideDirection * 2000.0f; // Ajusta este valor según sea necesario
-		GetCharacterMovement()->AddForce(Force);
-	}
+    SkateBoard = Cast<UStaticMeshComponent>(GetDefaultSubobjectByName(TEXT("SkateStaticMesh")));
+    
+    if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+    {
+        if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+        {
+            Subsystem->AddMappingContext(DefaultMappingContext, 0);
+        }
+    }
 }
+
+//Called every frame. Updates movement input, applies sliding force, and adjusts the character's rotation to match the slope.
 void ASkateboardingSimCharacter::Tick(float DeltaSeconds)
 {
-	const FVector ForwardDirection = SkateBoard->GetRightVector();
-	MF_Value = FMath::Lerp(MF_Value, TargetSkateVelocity, 0.01f);
-
-	AddMovementInput(ForwardDirection, MF_Value);
-	ApplySlidingForce();
-	AdjustRotationToSlope(DeltaSeconds);
-	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Yellow,FString::Printf( TEXT("Supone que hay pendiente: %f"),GetCharacterMovement()->GroundFriction));
+    const FVector ForwardDirection = SkateBoard->GetRightVector();
+    MF_Value = FMath::Lerp(MF_Value, TargetSkateVelocity, 0.01f);
+    MF_Value = FMath::Clamp(MF_Value, -FLT_MAX, 0.0f);
+    
+    AddMovementInput(ForwardDirection, MF_Value);
+    ApplySlidingForce();
+    AdjustRotationToSlope(DeltaSeconds);
 }
 
 //////////////////////////////////////////////////////////////////////////
 // Input
-
+//Binds the input actions for jumping, moving, and looking to their corresponding functions.
 void ASkateboardingSimCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
-	// Set up action bindings
-	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent)) {
-		
-		// Jumping
-		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &ASkateboardingSimCharacter::StartJumping);
-		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &ACharacter::Jump);
-		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
+    if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent)) 
+    {
+        EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &ASkateboardingSimCharacter::StartJumping);
+        EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &ACharacter::Jump);
+        EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
 
-		// Moving
-		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Started, this, &ASkateboardingSimCharacter::StartMove);
-		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ASkateboardingSimCharacter::Move);
-		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Completed, this, &ASkateboardingSimCharacter::StopMove);
+        EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Started, this, &ASkateboardingSimCharacter::StartMove);
+        EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ASkateboardingSimCharacter::Move);
+        EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Completed, this, &ASkateboardingSimCharacter::StopMove);
 
-		// Looking
-		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ASkateboardingSimCharacter::Look);
-	}
-	else
-	{
-		UE_LOG(LogTemplateCharacter, Error, TEXT("'%s' Failed to find an Enhanced Input component! This template is built to use the Enhanced Input system. If you intend to use the legacy system, then you will need to update this C++ file."), *GetNameSafe(this));
-	}
+        EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ASkateboardingSimCharacter::Look);
+    }
 }
 
+//Handles the movement input from the player, setting the target velocity and direction based on input.
 void ASkateboardingSimCharacter::Move(const FInputActionValue& Value)
 {
-	// input is a Vector2D
-	FVector2D MovementVector = Value.Get<FVector2D>();
+    FVector2D MovementVector = Value.Get<FVector2D>();
 
-	if (Controller != nullptr)
-	{
-		const FRotator Rotation = Controller->GetControlRotation();
-		const FRotator YawRotation(0, Rotation.Yaw, 0);
-		const FVector RightDirection = SkateBoard->GetForwardVector();
+    if (Controller != nullptr)
+    {
+        const FVector RightDirection = SkateBoard->GetForwardVector();
 
-		TargetSkateVelocity = MovementVector.Y;
+        TargetSkateVelocity = MovementVector.Y;
+
+        AddMovementInput(RightDirection, MovementVector.X * 0.02f);
         
-
-		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, FString::Printf(TEXT("MoveForwardValue: %f"), MF_Value));
-		// add movement 
-		AddMovementInput(RightDirection, MovementVector.X*0.02f);
-
-		MoveForwardValue = MovementVector.Y;
-		MoveRightValue = MovementVector.X;
-	}
+        MoveForwardValue = MovementVector.Y;
+        MoveRightValue = MovementVector.X;
+    }
 }
 
+//Handles the look input from the player, allowing the character to rotate based on mouse or controller input.
 void ASkateboardingSimCharacter::Look(const FInputActionValue& Value)
 {
-	// input is a Vector2D
-	FVector2D LookAxisVector = Value.Get<FVector2D>();
+    FVector2D LookAxisVector = Value.Get<FVector2D>();
 
-	if (Controller != nullptr)
-	{
-		// add yaw and pitch input to controller
-		AddControllerYawInput(LookAxisVector.X);
-		AddControllerPitchInput(LookAxisVector.Y);
-	}
+    if (Controller != nullptr)
+    {
+        AddControllerYawInput(LookAxisVector.X);
+        AddControllerPitchInput(LookAxisVector.Y);
+    }
 }
 
+//Returns the current movement input values for forward and right directions.
 FVector2D ASkateboardingSimCharacter::GetUserMyInputs() const
 {
-	return FVector2D(MoveForwardValue, MoveRightValue);
+    return FVector2D(MoveForwardValue, MoveRightValue);
 }
 
+//Called when movement input starts. Adjusts character movement settings based on input direction.
 void ASkateboardingSimCharacter::StartMove(const FInputActionValue& Value)
 {
-	FVector2D MovementVector = Value.Get<FVector2D>();
-	UE_LOG(LogTemplateCharacter, Log, TEXT("Move input started"));
-	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Yellow, TEXT("Move input started"));
-	if(MovementVector.Y!=0)
-	{
-		if(MovementVector.Y<0)
-			isBoosting = true;
-		GetCharacterMovement()->BrakingDecelerationWalking = 3000.f;
-		GetCharacterMovement()->GroundFriction = 8.0f;
-	}
+    FVector2D MovementVector = Value.Get<FVector2D>();
+    if (MovementVector.Y != 0)
+    {
+        if (MovementVector.Y < 0)
+            isBoosting = true;
+        GetCharacterMovement()->BrakingDecelerationWalking = 3000.f;
+        GetCharacterMovement()->GroundFriction = 8.0f;
+    }
 }
 
+//Called when movement input stops. Resets the target velocity and boosting state.
 void ASkateboardingSimCharacter::StopMove()
 {
-	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Yellow, TEXT("Move input finished"));
-	TargetSkateVelocity = 0.0f;
-	isBoosting = false;
-	//MF_Value = 0.0f;
+    TargetSkateVelocity = 0.0f;
+    isBoosting = false;
 }
+
+//Sets the jumping state to true when the jump input is received.
 void ASkateboardingSimCharacter::StartJumping()
 {
-	isJumping = false;
-	isJumping = true;
-	
+    isJumping = true;
 }
 
+//Activates slow motion by reducing the global time dilation for a short duration.
 void ASkateboardingSimCharacter::SlowDownTime()
 {
-	UGameplayStatics::SetGlobalTimeDilation(GetWorld(), 0.3f);
-	GetWorldTimerManager().SetTimer(TimerHandle_SlowMotion, this, &ASkateboardingSimCharacter::RestoreNormalSpeed, 0.5f, false);
+    UGameplayStatics::SetGlobalTimeDilation(GetWorld(), 0.3f);
+    GetWorldTimerManager().SetTimer(TimerHandle_SlowMotion, this, &ASkateboardingSimCharacter::RestoreNormalSpeed, 0.5f, false);
 }
 
-
+//Restores the global time dilation to normal speed after slow motion ends.
 void ASkateboardingSimCharacter::RestoreNormalSpeed()
 {
-	UGameplayStatics::SetGlobalTimeDilation(GetWorld(), 1.0f);
+    UGameplayStatics::SetGlobalTimeDilation(GetWorld(), 1.0f);
 }
 
+//Applies a sliding force to the character when on a slope, simulating the effect of gravity on an incline.
 void ASkateboardingSimCharacter::ApplySlidingForce()
 {
-	if (!isBoosting) // Solo aplicar la fuerza de deslizamiento si no se están presionando los botones de movimiento
-	{
-		FVector FloorNormal = GetCharacterMovement()->CurrentFloor.HitResult.ImpactNormal;
-        
-		// Si la pendiente es suficientemente inclinada, aplicar fuerza de deslizamiento
-		if (FloorNormal.Z < 1.0f)  // 1.0f significa completamente plano, menor es una pendiente
-		{
-			GetCharacterMovement()->BrakingDecelerationWalking = 100.f;
-			GetCharacterMovement()->GroundFriction = 1.0f;
-			FVector Gravity = FVector(0.0f, 0.0f, -980.0f);  // Gravedad estándar en Unreal Engine
-			FVector GravityParallel = Gravity - (FloorNormal * FVector::DotProduct(Gravity, FloorNormal));  // Componente paralela de la gravedad
-			FVector SlideDirection = GravityParallel.GetSafeNormal();
+    if (!isBoosting)
+    {
+        FVector FloorNormal = GetCharacterMovement()->CurrentFloor.HitResult.ImpactNormal;
 
-			// Aplicar fuerza de deslizamiento
-			GetCharacterMovement()->AddForce(SlideDirection * GetCharacterMovement()->Mass * 300.0f);  // Ajusta el multiplicador según sea necesario
-		}
-	}
+        if (FloorNormal.Z < 1.0f)
+        {
+            GetCharacterMovement()->BrakingDecelerationWalking = 100.f;
+            GetCharacterMovement()->GroundFriction = 1.0f;
+
+            FVector Gravity = FVector(0.0f, 0.0f, -980.0f);
+            FVector GravityParallel = Gravity - (FloorNormal * FVector::DotProduct(Gravity, FloorNormal));
+            FVector SlideDirection = GravityParallel.GetSafeNormal();
+
+            GetCharacterMovement()->AddForce(SlideDirection * GetCharacterMovement()->Mass * 300.0f);
+        }
+    }
 }
 
+//Adjusts the character's rotation to align with the slope of the surface they are moving on.
 void ASkateboardingSimCharacter::AdjustRotationToSlope(float DeltaTime)
 {
-	// Obtener la normal del suelo
-	FVector FloorNormal = GetCharacterMovement()->CurrentFloor.HitResult.ImpactNormal;
+    FVector FloorNormal = GetCharacterMovement()->CurrentFloor.HitResult.ImpactNormal;
 
-	// Si la normal del suelo es válida
-	if (FloorNormal.Z < 1.0f)
-	{
-		// Calcular la nueva rotación basada en la normal del suelo
-		FRotator TargetSlopeRotation = FRotationMatrix::MakeFromZX(FloorNormal, GetActorForwardVector()).Rotator();
+    if (FloorNormal.Z < 1.0f)
+    {
+        FRotator TargetSlopeRotation = FRotationMatrix::MakeFromZX(FloorNormal, GetActorForwardVector()).Rotator();
 
-		// Determinar la dirección del movimiento
-		FVector ForwardVector = GetActorForwardVector();
-		FVector MovementDirection = GetVelocity().GetSafeNormal();
+        FVector ForwardVector = GetActorForwardVector();
+        FVector MovementDirection = GetVelocity().GetSafeNormal();
+        FVector ProjectedMovement = FVector::VectorPlaneProject(MovementDirection, FloorNormal);
 
-		// Proyectar el vector de movimiento en el plano de la pendiente
-		FVector ProjectedMovement = FVector::VectorPlaneProject(MovementDirection, FloorNormal);
+        float DotProduct = FVector::DotProduct(ForwardVector, ProjectedMovement);
+        bool bIsMovingUpSlope = DotProduct > 0;
 
-		// Determinar si el personaje está subiendo o bajando
-		float DotProduct = FVector::DotProduct(ForwardVector, ProjectedMovement);
-		bool bIsMovingUpSlope = DotProduct > 0;
+        float RotationOffsetPitch = 150.0f;
 
-		// Agregar un offset adicional a la rotación objetivo
-		float RotationOffsetPitch = 130.0f; // Ajusta este valor para cambiar el offset en el eje de inclinación (pitch)
+        if (bIsMovingUpSlope)
+        {
+            TargetSlopeRotation.Pitch += RotationOffsetPitch;
+        }
+        else
+        {
+            TargetSlopeRotation.Pitch -= RotationOffsetPitch;
+        }
 
-		if (bIsMovingUpSlope)
-		{
-			TargetSlopeRotation.Pitch += RotationOffsetPitch;
-		}
-		else
-		{
-			TargetSlopeRotation.Pitch -= RotationOffsetPitch;
-		}
+        FRotator NewRotation = FMath::RInterpTo(GetActorRotation(), TargetSlopeRotation, DeltaTime, 2.5f);
 
-		// Interpolar entre la rotación actual y la rotación objetivo
-		FRotator NewRotation = FMath::RInterpTo(GetActorRotation(), TargetSlopeRotation, DeltaTime, 5.0f); // Ajusta el valor 5.0f para cambiar la velocidad de la interpolación
-
-		// Aplicar la nueva rotación al personaje
-		SetActorRotation(NewRotation);
-	}
+        SetActorRotation(NewRotation);
+    }
 }
-
-
-
--- Source/SkateboardingSim/SkateboardingSimCharacter.h
+++ Source/SkateboardingSim/SkateboardingSimCharacter.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "Logging/LogMacros.h"
+#include "Sound/SoundCue.h"
 #include "SkateboardingSimCharacter.generated.h"
 
 class USpringArmComponent;
@@ -20,6 +21,7 @@ class ASkateboardingSimCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
+private:
 	/** Camera boom positioning the camera behind the character */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
 	USpringArmComponent* CameraBoom;
@@ -44,75 +46,88 @@ class ASkateboardingSimCharacter : public ACharacter
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
 	UInputAction* LookAction;
 
-	/** Push Input Action */
+	/** Boost Input Action */
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
 	UInputAction* BoostAction;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "SkatingTools", meta = (AllowPrivateAccess = "true") )
 	UStaticMeshComponent* SkateBoard;
-	//USkeletalMeshComponent* SkateBoard;
 
+	/** Background music */
+	UPROPERTY(EditAnywhere, Category = "Audio")
+	USoundBase* BackgroundMusic;
 
-	
+	/** Audio Component */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Audio", meta = (AllowPrivateAccess = "true"))
+	class UAudioComponent* AudioComponent;
 
-public:
-	ASkateboardingSimCharacter();
-	// Función para obtener las entradas del usuario
-	UFUNCTION(BlueprintCallable, Category = "Input")
-	FVector2D GetUserMyInputs() const;
+	/** Timer handle for slow motion effect */
+	FTimerHandle TimerHandle_SlowMotion;
 
-	UFUNCTION(BlueprintCallable, Category = "SlowDown")
-	void SlowDownTime();
-	
-	UPROPERTY(BlueprintReadOnly)
-	bool isBoosting = false;
-
-	UPROPERTY(BlueprintReadWrite)
-	bool isJumping = false;
-
-protected:
-
-	void StartMove(const FInputActionValue& Value);
-
-	void StartJumping();
-	
-	/** Called for movement input */
-	void Move(const FInputActionValue& Value);
-
-	/** Called for looking input */
-	void Look(const FInputActionValue& Value);
-
-	void StopMove();
-
-	void AdjustRotationToSlope(float DeltaTime);
-
-	void RestoreNormalSpeed();
-	
-	void ApplySlidingForce();
-
-	// Variables para almacenar los valores de movimiento
+	/** Variables para almacenar los valores de movimiento */
 	float MoveForwardValue;
 	float MoveRightValue;
 	float CurrentForwardSpeed;
 	float MF_Value = 0.0f;
 	float TargetSkateVelocity = 0.0f;
 
-	// Timer handle for slow motion effect
-	FTimerHandle TimerHandle_SlowMotion;
-
 protected:
-	// APawn interface
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-	
-	// To add mapping context
-	virtual void BeginPlay();
+	/** Called when the game starts or when spawned */
+	virtual void BeginPlay() override;
 
+	/** Called every frame */
 	virtual void Tick(float DeltaSeconds) override;
 
+	/** Set up player input component */
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	/** Handles starting movement input */
+	void StartMove(const FInputActionValue& Value);
+
+	/** Handles starting jump input */
+	void StartJumping();
+	
+	/** Handles movement input */
+	void Move(const FInputActionValue& Value);
+
+	/** Handles looking input */
+	void Look(const FInputActionValue& Value);
+
+	/** Handles stopping movement input */
+	void StopMove();
+
+	/** Adjusts the character's rotation to match the slope */
+	void AdjustRotationToSlope(float DeltaTime);
+
+	/** Restores normal speed after slow motion */
+	void RestoreNormalSpeed();
+	
+	/** Applies sliding force when on a slope */
+	void ApplySlidingForce();
+
 public:
-	/** Returns CameraBoom subobject **/
-	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
-	/** Returns FollowCamera subobject **/
-	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+	ASkateboardingSimCharacter();
+
+	/** Returns CameraBoom subobject */
+	FORCEINLINE USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+
+	/** Returns FollowCamera subobject */
+	FORCEINLINE UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+
+	/** Returns user inputs */
+	UFUNCTION(BlueprintCallable, Category = "Input")
+	FVector2D GetUserMyInputs() const;
+
+	/** Activates slow down time effect */
+	UFUNCTION(BlueprintCallable, Category = "SlowDown")
+	void SlowDownTime();
+
+	/** Indicates if the character is boosting */
+	UPROPERTY(BlueprintReadOnly)
+	bool isBoosting = false;
+
+	/** Indicates if the character is jumping */
+	UPROPERTY(BlueprintReadWrite)
+	bool isJumping = false;
 };
 
